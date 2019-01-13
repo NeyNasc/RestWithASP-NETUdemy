21c2f72bc3652d2f1b88e6df4316ef4474147098
claudiney <claudineydelima@gmail.com>
2019-01-13 19:27:30 -0200

    Retirando mocks

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace RestWithASPNETUdemy
 {
@@ -14,10 +7,10 @@ namespace RestWithASPNETUdemy
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
--- RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImpl.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImpl.cs
@@ -4,64 +4,84 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
 
 namespace RestWithASPNETUdemy.Services.Implementations
 {
     public class PersonServiceImpl : IPersonService
     {
-        private volatile int count;
+        private MySqlContext _context;
+
+        public PersonServiceImpl(MySqlContext context)
+        {
+            _context = context;
+        }
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
             return person;
         }
         public Person FindById(long id)
         {
-            return new Person
-            {
-                Id = 1,
-                FirstName = "Claudiney",
-                LastName = "Nascimento",
-                Address = "Longe do Brasil",
-                Gender = "Male"
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public void Delete(int id)
         {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            try
+            {
+                if (result != null)
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public List<Person> FindAll()
         {
-            List<Person> persons = new List<Person>();
-            for (int i = 0; i < 8; i++)
-            {
-                Person person = MockPersons(i);
-                persons.Add(person);
-            }
-            return persons;
+            return _context.Persons.ToList();
         }
 
         public Person Update(Person person)
         {
+            if (!Exist(person.Id)) return new Person();       
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
             return person;
         }
 
-        private Person MockPersons(int i)
+        public bool Exist(long? id)
         {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Person " + i,
-                LastName = "LastNamet",
-                Address = "Some Address",
-                Gender = "Not say"
-            };
-        }
 
-        private long IncrementAndGet()
-        {
-            return Interlocked.Increment(ref count);
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
